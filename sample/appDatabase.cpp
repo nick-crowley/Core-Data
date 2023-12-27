25d75e2845da011edd0e7f10fa67cefbae910770
nick-crowley <nicholas.crowley@gmail.com>
2023-12-27 20:28:08 +0000

    test: Implemented `appDatabase` sample to demonstrate creating, populating, and querying a database

--- sample/appDatabase.cpp
+++ sample/appDatabase.cpp
@@ -3,7 +3,7 @@
 using namespace core;
 
 // Define program meta-data
-//metadata std::string_view meta::Settings<program_name> = "Core.Data Database I/O Application";
+//metadata std::string_view meta::Settings<program_name> = "Core.Data Simple database Application";
 //metadata std::string_view meta::Settings<program_version> = "Demo";
 
 //! @brief  Entry-point
@@ -13,9 +13,25 @@ try {
 	// Create log-file only if user provided "-log" commandline switch
 	if (cmdLine.contains(L"-log")) 
 		clog.createLogFile("appDatabase.log");
+	else
+		clog.attach(std::cout);
 	startupBanner();
 	
-	// TODO:
+	clog << Important{"Creating in-memory database"};
+	using enum data::ConnectionFlags;
+	data::Connection db = data::Connection::open("whatever.db", Create|Memory|ReadWrite);
+
+	clog << Important{"Creating 'Employees' table"};
+	db.execute("CREATE TABLE IF NOT EXISTS Employees (ID INTEGER PRIMARY KEY,"
+                                                     "Name TEXT NOT NULL,"
+                                                     "Salary REAL)");
+	db.execute("INSERT INTO Employees VALUES ('1', 'Darren', '24000')");
+	db.execute("INSERT INTO Employees VALUES ('2', 'Susan', '55000')");
+
+	clog << Important{"Querying database..."};
+	for (auto [name,salary] : db.execute<std::string,float>("SELECT Name, Salary FROM Employees"))
+		clog << Important{"-> Found employee: name={} salary={}", name, salary};
+
 	return 0;
 }
 catch (std::exception const& e)
@@ -24,7 +40,6 @@ catch (std::exception const& e)
 	return -1;
 }
 
-// Entry point
 int 
 wmain(int argc, wchar_t* argv[]) {
 	return ::run(argc > 1 ? argv[1] : L"");
